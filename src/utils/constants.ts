a59569adc507bcfeba3fc464cea3ab7561b3e428
emelcd <mick.altura@gmail.com>
2023-12-06 00:31:40 +0100

    updated template

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -20,7 +20,6 @@ export const TEMPLATE_EMAIL_HTML = `
       </tr>
       <tr>
         <th style="border: 1px solid gray; padding: 10px; text-align: center">
-          <div style="background-color: #f2f2f2">
             <a
               href="https://www.youtube.com/watch?v={{videoID}}"
               target="_blank"
@@ -30,27 +29,29 @@ export const TEMPLATE_EMAIL_HTML = `
                 alt="video"
               />
             </a>
-          </div>
         </th>
       </tr>
       <tr>
         <td style="padding: 10px">
           <p>{{videoDescription}}</p>
           <hr />
-          <p>
+          <p style="font-size: 10px">
             <strong>Disclaimer:</strong> This is an automated email. Please do
             not reply to this email. 
             <hr>
-            <code>
+            <code style="font-size: 10px">
               If you want to know how this script is
               built check the <a href="https://github.com/emelcd/mailing_list_apps_script">source code</a>.
             </code>
           </p>
+          <!-- add copyrigth -->
+          <footer style="font-size: 10px; text-align: right; margin-top: 20px; padding: 0px">
+            <p>&copy; 2023 <a href="https://github.com/emelcd">emelcd</a>. All rights reserved.
+            </p>
+        </footer>
         </td>
       </tr>
     </table>
   </body>
 </html>
-
-
 `
